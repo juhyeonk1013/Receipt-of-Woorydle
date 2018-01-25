d5fdfa3a8c680f70464ab59639fa8f2e2ef61c4e
JAEYEON\jaeyeon <wodus7841@naver.com>
2018-01-25 17:53:36 +0900

    팀 추가 버튼 활성화

--- ReceiptofWoorydle/app/src/main/java/com/flowerroad/receiptofwoorydle/MainActivity.java
+++ ReceiptofWoorydle/app/src/main/java/com/flowerroad/receiptofwoorydle/MainActivity.java
@@ -91,6 +91,15 @@ public class MainActivity extends AppCompatActivity {
         }catch(InterruptedException e){
 
         }
+        Button btn2 = (Button) findViewById(R.id.button);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReceiptListActivity.class);
+                startActivity(intent);
+            }
+        });
+
         btn = (Button) findViewById(R.id.receipt_detect);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
