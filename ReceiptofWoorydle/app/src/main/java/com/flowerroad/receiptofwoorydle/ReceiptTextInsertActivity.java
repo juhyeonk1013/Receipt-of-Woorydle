fbab0364b7fa5c7d72a062d658044f02cba36348
JAEYEON\jaeyeon <wodus7841@naver.com>
2018-01-25 22:12:21 +0900

    Google places 키 추가, 장소 자동입력 구현

--- ReceiptofWoorydle/app/src/main/java/com/flowerroad/receiptofwoorydle/ReceiptTextInsertActivity.java
+++ ReceiptofWoorydle/app/src/main/java/com/flowerroad/receiptofwoorydle/ReceiptTextInsertActivity.java
@@ -1,21 +1,82 @@
 package com.flowerroad.receiptofwoorydle;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.support.v4.app.FragmentActivity;
 /**
  * Created by jaeyeon on 2018-01-25.
  */
 
-public class ReceiptTextInsertActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+public class ReceiptTextInsertActivity extends FragmentActivity  {
 
-        //@Override
+    private GoogleApiClient mGoogleApiClient;
+
+    //@Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt_text_insert);
+
+        Button btn1 = (Button) findViewById(R.id.date_btn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dpd = new DatePickerDialog
+                        (ReceiptTextInsertActivity.this, // 현재화면의 제어권자
+                                new DatePickerDialog.OnDateSetListener() {
+                                    public void onDateSet(DatePicker view,
+                                                          int year, int monthOfYear,int dayOfMonth) {
+                                        Toast.makeText(getApplicationContext(),
+                                                year+"년 "+(monthOfYear+1)+"월 "+dayOfMonth+"일 을 선택했습니다",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                , // 사용자가 날짜설정 후 다이얼로그 빠져나올때
+                                //    호출할 리스너 등록
+                                2015, 6, 21); // 기본값 연월일
+            }
+        });
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("ReceiptTextInsert", "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("ReceiptTextInsert", "An error occurred: " + status);
+            }
+        });
+
+
 
         Button btn = (Button) findViewById(R.id.finish);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +86,21 @@ public class ReceiptTextInsertActivity extends AppCompatActivity {
                 startActivity(TextDetectionIntent);
             }
         });
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
     }
+
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        LatLng sydney = new LatLng(-33.852, 151.211);
+//        googleMap.addMarker(new MarkerOptions().position(sydney)
+//                .title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//
+//    }
+
 
 }
